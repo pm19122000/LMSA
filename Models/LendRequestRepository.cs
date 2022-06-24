cf243c51fdd306870de5083f98cc4c78920d8f68
pm19122000 <62829358+pm19122000@users.noreply.github.com>
2022-06-24 14:42:17 +0530

    Add files via upload

---
+++ Models/LendRequestRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+namespace LibraryManagmentSystem.Models
+{
+    public class LendRequestRepository : ILendRequestRepository
+    {
+        private readonly LibraryManagementContext _libraryManagementContext;
+
+        public LendRequestRepository(LibraryManagementContext libraryManagementContext)
+        {
+            _libraryManagementContext = libraryManagementContext;
+        }
+
+        public IEnumerable<LendRequest> GetAllLendRequest
+        {
+            get
+            {
+                return _libraryManagementContext.LendRequests.Include(b=>b.BooksInfo).Where(l=>l.LendStatus == "Requested");
+            }
+        }
+        public IEnumerable<LendRequest> GetlendRequestbyId(int Id)
+        {
+            return _libraryManagementContext.LendRequests.Include(b => b.BooksInfo).Where(u => u.UserId == Id);
+        }
+        public LendRequest GetLendRequestByLendId(int LendId)
+        {
+            return _libraryManagementContext.LendRequests.Include(b => b.BooksInfo).FirstOrDefault(l=> l.LendId == LendId);
+        }
+        
+    }
+}
